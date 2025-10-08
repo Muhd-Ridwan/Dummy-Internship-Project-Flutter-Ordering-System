d5dae5d46d37c5c95434aff9929210c3e78ef64d
Muhammad Ridwan <157098120+Muhd-Ridwan@users.noreply.github.com>
2025-10-08 18:27:33 +0800

    Adding new file of registration.dart
    Not yet complete the UI of registration.dart

---
+++ frontend/ordering_system/lib/authentication/registration.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key})
+
+  @override
+  State<Register> createState () => _Register();
+}
+
+class _Register extends State<Register> {
+  final TextEditingController
+}
